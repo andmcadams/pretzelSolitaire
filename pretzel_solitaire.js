63ffebcb7d19982ee2190294da219e4918309cfb
Andrew McAdams <andmcadams@gmail.com>
2017-06-30 18:26:46 -0500

    Add in functionality to move cards around

--- pretzel_solitaire.js
+++ pretzel_solitaire.js
@@ -1,19 +1,31 @@
 var lastClicked;
-var suits = ["hearts", "clubs", "diamonds", "spades"];
-var cards = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "blank"];
+var lastClickedVal = '';
+var suits = ["spades", "hearts", "diamonds", "clubs"];
+var cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "blank"];
 var deck = cards.map(function (e, i) {
     return [[suits[0], e], [suits[1], e], [suits[2], e], [suits[3], e]];
 });
 deck = [].concat.apply([], deck);
 deck = shuffleDeck(deck);
-var grid = clickableGrid(4,14,function(el,row,col,i){
+var grid = clickableGrid(4,14,function(el,row,col,i,val){
     console.log("You clicked on element:",el);
     console.log("You clicked on row:",row);
     console.log("You clicked on col:",col);
     console.log("You clicked on item #:",i);
 
     el.className='clicked';
-    if (lastClicked) lastClicked.className='';
+    if (lastClicked) 
+  	{
+  		if(val == 'blank')
+  		{
+  			el.innerHTML = lastClicked.innerHTML;
+  			lastClicked.innerHTML = "";
+  			lastClickedVal = '';
+  		}
+  		lastClicked.className='';
+  		console.log("Value of last: ", lastClickedVal);
+  	}
+  	lastClickedVal = val;
     lastClicked = el;
 });
 
@@ -39,13 +51,27 @@ function clickableGrid( rows, cols, callback ){
         for (var c=0;c<cols;++c){
             var cell = tr.appendChild(document.createElement('td'));
             console.log('Col:', deck);
-            if(deck[r*cols + c][1] != "blank")
-            	cell.innerHTML = '<img src="SVG-cards-1.3/' + deck[r*cols + c][1] + '_of_' + deck[r*cols + c][0] + '.svg" alt="two" class="card"></img>';
-            cell.addEventListener('click',(function(el,r,c,i){
+            var val = 'test';
+            if(c != 0)
+            {
+            	if(deck[r*(cols-1) + c-1][1] != "blank")
+            	{
+            		cell.innerHTML = '<img src="SVG-cards-1.3/' + deck[r*(cols-1) + c-1][1] + '_of_' + deck[r*(cols-1) + c-1][0] + '.svg" class="card"></img>';
+            		val = deck[r*(cols-1) + c-1][1];
+            	}
+            	else
+            		val = "blank";
+            }
+            else
+            {
+            	cell.innerHTML = '<img src="SVG-cards-1.3/ace_of_' + suits[r] + '.svg" class="card"></img>';
+            	val = "ace";
+            }
+            cell.addEventListener('click',(function(el,r,c,i,val){
                 return function(){
-                    callback(el,r,c,i);
+                    callback(el,r,c,i,val);
                 }
-            })(cell,r,c,i),false);
+            })(cell,r,c,i,val),false);
         }
     }
     return grid;
