05889c8bb576c532d55fd3f12587265620aae9be
Andrew McAdams <andmcadams@gmail.com>
2017-06-30 20:46:20 -0500

    Correct card movement so blank cards cannot be moved

--- pretzel_solitaire.js
+++ pretzel_solitaire.js
@@ -1,5 +1,6 @@
 var lastClicked;
 var lastClickedVal = '';
+var lastIndex;
 var suits = ["spades", "hearts", "diamonds", "clubs"];
 var cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "blank"];
 var deck = cards.map(function (e, i) {
@@ -7,26 +8,43 @@ var deck = cards.map(function (e, i) {
 });
 deck = [].concat.apply([], deck);
 deck = shuffleDeck(deck);
-var grid = clickableGrid(4,14,function(el,row,col,i,val){
+var grid = clickableGrid(4,14,function(el,row,col){
     console.log("You clicked on element:",el);
     console.log("You clicked on row:",row);
     console.log("You clicked on col:",col);
-    console.log("You clicked on item #:",i);
-
-    el.className='clicked';
-    if (lastClicked) 
+    //if there is already something clicked
+    	//if the thing clicked is blank
+    		//switch the inner html, value, and index attributes
+    		//set neither to clicked
+    //if nothing is clicked
+    	//highlight it and set it to lastClicked
+    if (lastClicked && lastClicked != el) 
   	{
-  		if(val == 'blank')
+  		if(el.getAttribute('value') == 'blank')
   		{
+  			var tempIndex = el.getAttribute('index');
+
   			el.innerHTML = lastClicked.innerHTML;
+  			el.setAttribute('value', lastClicked.getAttribute('value'));
+  			el.setAttribute('index', lastClicked.getAttribute('index'));
+  			el.className='';
+
+  			var temp = deck[tempIndex];
+  			deck[tempIndex] = deck[lastClicked.getAttribute('index')];
+  			deck[lastClicked.getAttribute('index')] = temp;
+
   			lastClicked.innerHTML = "";
-  			lastClickedVal = '';
+  			lastClicked.setAttribute('value', 'blank');
+  			lastClicked.setAttribute('index', tempIndex);
+	  		lastClicked.className='';
+	  		lastClicked = null;
   		}
-  		lastClicked.className='';
-  		console.log("Value of last: ", lastClickedVal);
   	}
-  	lastClickedVal = val;
-    lastClicked = el;
+  	else if(el.getAttribute('value') != "blank" && el.className != 'clicked')
+  	{
+  		el.className='clicked';
+    	lastClicked = el;
+  	}
 });
 
 document.body.appendChild(grid);
@@ -51,27 +69,32 @@ function clickableGrid( rows, cols, callback ){
         for (var c=0;c<cols;++c){
             var cell = tr.appendChild(document.createElement('td'));
             console.log('Col:', deck);
-            var val = 'test';
+						var val = document.createAttribute("value");       // Create a "class" attribute
+						var ind = document.createAttribute("index");       // Create a "class" attribute
             if(c != 0)
             {
             	if(deck[r*(cols-1) + c-1][1] != "blank")
             	{
             		cell.innerHTML = '<img src="SVG-cards-1.3/' + deck[r*(cols-1) + c-1][1] + '_of_' + deck[r*(cols-1) + c-1][0] + '.svg" class="card"></img>';
-            		val = deck[r*(cols-1) + c-1][1];
+            		val.value = deck[r*(cols-1) + c-1][1];
             	}
             	else
-            		val = "blank";
+            		val.value = "blank";
             }
             else
             {
             	cell.innerHTML = '<img src="SVG-cards-1.3/ace_of_' + suits[r] + '.svg" class="card"></img>';
-            	val = "ace";
+            	val.value = "ace";
             }
-            cell.addEventListener('click',(function(el,r,c,i,val){
+            ind.value = r*(cols-1) + c-1;
+            cell.setAttributeNode(val);                          // Add the class attribute to <h1>
+            cell.setAttributeNode(ind);                          // Add the class attribute to <h1>
+
+            cell.addEventListener('click',(function(el,r,c){
                 return function(){
-                    callback(el,r,c,i,val);
+                    callback(el,r,c);
                 }
-            })(cell,r,c,i,val),false);
+            })(cell,r,c),false);
         }
     }
     return grid;
